0aff897b02c38b97515c007a48fd4dfa699d01da
dmaxadu <dmaxadu@ufrj.br>
2022-03-15 17:29:03 -0300

    Side scroll almost done

--- src/components/Movie Row/MovieRow.js
+++ src/components/Movie Row/MovieRow.js
@@ -4,25 +4,33 @@ import './MovieRow.css';
 // import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default ({title, items}) => {
+    const urlRight = "https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-right.png";
+    const urlLeft = "https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-left.png";
     return (
         <div className="movieRow">
-            <h2>{title}</h2>
-            {/* <div className="movieRow--left">
-                <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-left.png"/>
-            </div>
-            <div className="movieRow--right">
-                <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-right.png"/>
-            </div> */}
+        <h2>{title}</h2>
+        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+           <div className="movieRow--left">
+               <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-left.png"/>
+           </div>
+   
 
-            <div className="movieRow--listarea">
-                <div className="movieRow--list">
-                    {items.results?.length > 0 && items.results.map((item, key)=>(
-                        <div key={key} className="movieRow--item">
-                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
-                        </div>
-                    )) }
-                </div>
-            </div>
+           <div className="movieRow--listarea">
+               <div className="movieRow--list">
+                   {items.results?.length > 0 && items.results.map((item, key)=>(
+                       <div key={key} className="movieRow--item">
+                          <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
+                       </div>
+                   )) }
+               </div>
+           </div>
+
+
+             <div className="movieRow--right">
+               <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-right.png"/>
+           </div>
         </div>
+    </div>
+
     );
 };
