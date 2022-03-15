cbbe3a263735423f7e2930afae5f1d3cf726fd18
dmaxadu <dmaxadu@ufrj.br>
2022-03-15 16:58:57 -0300

    Almost done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/Movie Row/MovieRow';
+import FeaturedMovie from './components/Featured Movie/FeaturedMovie';
+import Header from './components/Header/Header';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default () => {
+
+    const [movieList, setMovieList] =  useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
+    const [blackHeader, setBalckHeader] = useState(false);
+
+    useEffect(()=>{
+        const loadAll = async () => {
+            /* pega a lista TODA de filmes */
+            let list = await Tmdb.getHomeList();
+            setMovieList(list);
+
+            /* pega o filme em DESTAQUE */
+            let originals = list.filter(i=>i.slug === 'originals');
+            let randomChoosen = Math.floor(Math.random() * (originals[0].items.results?.length - 1));
+            let choosen = originals[0].items.results[randomChoosen]
+            let choosenInfo = await Tmdb.getMovieInfo(choosen.id, 'tv');
+            setFeaturedData(choosenInfo);
+    }
+
+    loadAll();
+   }, []); 
+
+   useEffect(()=>{
+       const scrollListener = () => {
+           if(window.scrollY > 10){
+               setBalckHeader(true);
+           } else {
+               setBalckHeader(false);
+           }
+       }
+
+       window.addEventListener('scroll', scrollListener);
+
+       return () => {
+           window.removeEventListener('scroll', scrollListener);
+       }
+   }, []);
+    return (
+        <div className="page">
+
+            <Header black={blackHeader} />
+
+            {featuredData &&
+                <FeaturedMovie item={featuredData} />
+            }
+
+            <section className="lists">
+                {movieList.map((item, key)=>(
+                    <MovieRow key={key} title={item.title} items={item.items}/>
+                ))}
+            </section>
+
+            <footer>
+                Feito com <span role="img" aria-label="coração">❤️</span> por <strong><a href="https://danielmachado.netlify.app" target="_blank">Daniel Machado</a></strong><br/>
+                Todos os direitos de imagem reservados para <a href="https://netflix.com" target="_blank"><strong>Netflix</strong></a><br/>
+                Todos os dados foram obtidos através do <a href="https://themoviedb.org" target="_blank"><strong>The Movie DB</strong></a>
+            </footer>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/components/Movie Row/MovieRow.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './MovieRow.css';
+// import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+// import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+
+export default ({title, items}) => {
+    return (
+        <div className="movieRow">
+            <h2>{title}</h2>
+            {/* <div className="movieRow--left">
+                <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-left.png"/>
+            </div>
+            <div className="movieRow--right">
+                <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-right.png"/>
+            </div> */}
+
+            <div className="movieRow--listarea">
+                <div className="movieRow--list">
+                    {items.results?.length > 0 && items.results.map((item, key)=>(
+                        <div key={key} className="movieRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
+                        </div>
+                    )) }
+                </div>
+            </div>
+        </div>
+    );
+};
